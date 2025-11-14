722aec4a8ee85357f062fcc6094d76bd0869dd12
Davis Vann Bennett <davis.v.bennett@gmail.com>
2025-11-14 15:17:46 +0100

    add multiscales extension

---
+++ src/eopf_geozarr/data_api/geozarr/multiscales.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+from typing import Literal, Self
+
+from pydantic import BaseModel, Field, model_validator
+from pydantic.experimental.missing_sentinel import MISSING
+from typing_extensions import TypedDict
+
+ConventionID = Literal["d35379db-88df-4056-af3a-620245f8e347"]
+
+
+class MultiscaleConvention(BaseModel):
+    version: Literal["0.1.0"]
+    schema_url: Literal[
+        "https://raw.githubusercontent.com/zarr-conventions/multiscales/refs/tags/v0.1.0/schema.json"
+    ] = Field(alias="schema")
+    name: Literal["multiscales"] = "multiscales"
+    description: str = "Multiscale layout of zarr datasets"
+    spec: Literal[
+        "https://github.com/zarr-conventions/geo-proj/blob/v0.1.0/README.md"
+    ] = "https://github.com/zarr-conventions/geo-proj/blob/v0.1.0/README.md"
+
+
+MultiscaleConventions = TypedDict(  # type: ignore[misc]
+    "MultiscaleConventions",
+    {"d35379db-88df-4056-af3a-620245f8e347": MultiscaleConvention},
+    closed=False,
+)
+
+
+class ConventionAttributes(BaseModel):
+    zarr_conventions_version: Literal["0.1.0"]
+    zarr_conventions: MultiscaleConventions
+
+
+class ScaleLevel(BaseModel):
+    group: str
+    from_group: str | MISSING = MISSING
+    translation: tuple[float, ...] | MISSING = MISSING
+    factors: tuple[float, ...] | MISSING = MISSING
+    scale: tuple[float, ...] | MISSING = MISSING
+    resampling_method: str | MISSING = MISSING
+
+    model_config = {"extra": "allow"}
+
+    @model_validator(mode="after")
+    def check_model(self: Self) -> Self:
+        if self.from_group is not MISSING and self.scale is MISSING:
+            raise ValueError(
+                f"from_group was set to {self.from_group}, but scale was unset. This is an error."
+            )
+        return self
+
+
+class Multiscales(BaseModel):
+    layout: tuple[ScaleLevel, ...]
+    resampling_method: str | MISSING = MISSING
+    model_config = {"extra": "allow"}
+
+
+class MultiscalesAttributes(ConventionAttributes):
+    multiscales: Multiscales
---
+++ tests/test_data_api/test_multiscales.py
@@ -0,0 +1,27 @@
+import jsondiff
+import pytest
+from pydantic import ValidationError
+from pydantic_zarr.core import tuplify_json
+
+from eopf_geozarr.data_api.geozarr.multiscales import Multiscales, ScaleLevel
+
+from .conftest import MULTISCALES_EXAMPLES
+
+
+@pytest.mark.parametrize(
+    "json_example", MULTISCALES_EXAMPLES.items(), ids=lambda v: v[0]
+)
+def test_multiscales_rt(json_example: tuple[str, dict[str, object]]) -> None:
+    _, value = json_example
+    value_tup = tuplify_json(value)
+    multi_json = value_tup["attributes"]["multiscales"]
+    model = Multiscales(**multi_json)
+    observed = model.model_dump()
+    expected = multi_json
+    assert jsondiff.diff(observed, expected) == {}
+
+
+def test_scale_level_from_group() -> None:
+    meta = {"group": "1", "from_group": "0"}
+    with pytest.raises(ValidationError):
+        ScaleLevel(**meta)
